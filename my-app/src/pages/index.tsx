77aaf5d3ff9a9b568091cb7e7c92bd023d9057a0
mfelipperd <felipperabelodurans@gmail.com>
2023-04-24 16:43:45 -0300

    remove tag

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -69,7 +69,6 @@ const link = <a href="https://www.expomultimix.com/">termos de uso</a>
 const terms = `Sim, eu aceito os ${link}`
 
 const form = <div className={style.maxWidth}>
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <Stack
       component="form"
       sx={{
