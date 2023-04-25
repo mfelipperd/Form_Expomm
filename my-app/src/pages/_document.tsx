f2368f0cb87f1c3fdbb6dd93aa8a0e5f3459226b
mfelipperd <felipperabelodurans@gmail.com>
2023-04-25 17:53:59 -0300

    remove meta tag

--- my-app/src/pages/_document.tsx
+++ my-app/src/pages/_document.tsx
@@ -3,7 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <Head />
       <body>
         <Main />
